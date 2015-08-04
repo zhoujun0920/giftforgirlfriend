4c3a60dc776cece1ddfe15756acc47bf77575e32
zhoujun0920 <zhoujun199009@hotmail.com>
2015-08-04 18:23:21 -0500

    first commit

---
+++ gift/loadingView.swift
@@ -0,0 +1,21 @@
+//
+//  loadingView.swift
+//  gift
+//
+//  Created by Jun Zhou on 8/2/15.
+//  Copyright (c) 2015 Jun Zhou. All rights reserved.
+//
+
+import UIKit
+
+class loadingView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
